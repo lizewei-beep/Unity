26a1749f0b47261e85174f002a745d3b7ddd5b8e
Minam1ya <2376786803@qq.com>
2023-11-08 20:26:16 +0800

    MAP

---
+++ Assets/script/BlueButton.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueButton : MonoBehaviour
+{
+    PlayerBlue playerScript;
+    private void Awake()
+    {
+        playerScript = GetComponentInParent<PlayerBlue>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)//检测触碰地板，与跳跃有协作
+    {
+        if (collision.tag == "Ground")
+        {
+            playerScript.canJump = true;
+            playerScript.myAnim.SetBool("Jump", false);
+        }
+    }
+}
